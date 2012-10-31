31098c2104c9b41ca9e8d2bb215755e2bac389b9
Jason Bracey <braceynet@gmail.com>
2012-10-30 19:48:55 -0700

    Created a rails server

---
+++ test/railsinstaller_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsintallerDemo::Application.config.secret_token = 'a8838b95c755e4c0f6f4d63e3e80f9290aeed518c4baa52f9586aee7f8fb18ef251c102ebe515d2a8f09180792728961db178985eccc23d14b57064698b646a2'
---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ea64856c2752cdbc7316b9505d7ec0fa2708ed1f79fdce225bc0555a88a140dcc06bc62ee05f1af2145e13c926274296156a9e67a80e3f92b9a6e08753eb1a28'
